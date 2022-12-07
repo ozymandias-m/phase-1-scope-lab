2c4bcffe28b675bb3ddced171081e8a4fe40048d
Brian Muriithi M <brianmuriithi71@gmail.com>
2022-12-07 13:07:12 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(parameter = 'maybe bob'){
+    return bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    const changeLeastFavoriteCustomer = 'Ayesha';
+    changeLeastFavoriteCustomer = 'bob'
+}
